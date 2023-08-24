ef7e7c4b212f7b972d55f541aed604f22a04d7f2
joaocerqueira <jbraga.cerq@gmail.com>
2023-08-24 09:36:11 -0300

    Added prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SampleModule } from './sample/sample.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SampleModule],
+  imports: [SampleModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
