905b3e54eede20b91cee55e6bb7116d58eb3ee86
joaocerqueira <jbraga.cerq@gmail.com>
2023-08-24 09:13:14 -0300

    Added Module and docker mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SamplesModule } from './samples/samples.module';
+import { SampleModule } from './sample/sample.module';
 
 @Module({
-  imports: [SamplesModule],
+  imports: [SampleModule],
   controllers: [AppController],
   providers: [AppService],
 })
